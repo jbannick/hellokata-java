96301d7607f64f2315928bb024e559e058635232
John Bannick <jbannick@7128.com>
2017-10-25 04:58:44 -0400

    Add files via upload

---
+++ src/assertions/HelloAssertions.java
@@ -0,0 +1,10 @@
+
+public class HelloAssertions {
+	public static void main(String...args) {
+		System.out.println("Hello Assertions!");
+		
+		String userID = null;
+		
+		assert userID != null;
+	}
+}

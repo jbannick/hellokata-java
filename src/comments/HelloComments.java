5f0e0e4de5c484ee7856de85283ee96faf0001b5
John Bannick <jbannick@7128.com>
2017-10-25 05:02:59 -0400

    Add files via upload

---
+++ src/comments/HelloComments.java
@@ -0,0 +1,16 @@
+
+public class HelloComments {
+	/**
+	 * This is a Javadoc comment.
+	 */
+	public static void main(String...args) {
+		
+		/*
+		 * This is a C Language style Block Implementation comment.
+		 */
+				
+		// This is a Single Line, Implementation comment.
+		
+		System.out.println("Hello Comments!"); 		
+	}
+}

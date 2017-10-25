e618f2a15b297805ade00e5804a07581c157d051
John Bannick <jbannick@7128.com>
2017-10-25 05:21:14 -0400

    Add files via upload

---
+++ src/guid/HelloGUID.java
@@ -0,0 +1,26 @@
+import java.util.UUID;
+
+public class HelloGUID {
+	public static void main(String...args) {
+		System.out.println("Hello GUID!");
+		
+		UUID uuid01 = UUID.randomUUID();
+		UUID uuid02 = UUID.fromString(uuid01.toString());
+		
+		UUID uuid03 = UUID.randomUUID();
+		
+		System.out.printf("uuid01: %s\n", uuid01);
+		System.out.printf("uuid02: %s\n", uuid02);
+		System.out.printf("uuid03: %s\n", uuid03);
+		
+		System.out.printf("uuid01 == uuid02 %s\n", uuid01 == uuid02);
+		System.out.printf("uuid01.equals(uuid02) %s\n", uuid01.equals(uuid02));
+
+		System.out.printf("uuid01 == uuid03 %s\n", uuid01 == uuid03);
+		System.out.printf("uuid01.equals(uuid03) %s\n", uuid01.equals(uuid03));
+
+		System.out.printf("uuid01.compareTo(uuid02) %s\n", uuid01.compareTo(uuid02));
+		System.out.printf("uuid01.compareTo(uuid03) %s\n", uuid01.compareTo(uuid03));
+		System.out.printf("uuid03.compareTo(uuid01) %s\n", uuid03.compareTo(uuid01));
+	}
+}

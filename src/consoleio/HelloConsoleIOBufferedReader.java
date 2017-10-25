41692bcb0134b31b370022de9b741932516ce107
John Bannick <jbannick@7128.com>
2017-10-25 05:04:54 -0400

    Add files via upload

---
+++ src/consoleio/HelloConsoleIOBufferedReader.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HelloConsoleIOBufferedReader {
+	public static void main(String...args) {
+		System.out.println("Hello Console IO Buffered Reader!");
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			System.out.println("Enter something: ");
+			String line = reader.readLine();
+			System.out.println(line);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}

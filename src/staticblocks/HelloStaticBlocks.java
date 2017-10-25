9d56120863d654b90140aa0c19cc5fae45d49c74
John Bannick <jbannick@7128.com>
2017-10-25 05:25:19 -0400

    Add files via upload

---
+++ src/staticblocks/HelloStaticBlocks.java
@@ -0,0 +1,27 @@
+
+public class HelloStaticBlocks {
+	
+	static {
+		System.out.println("Static block 1");
+		mValue1 = 123;
+	}
+	
+	static {
+		System.out.println("Static block 2");
+		setValue2(456);
+	}
+	
+	static int mValue1;
+	static int mValue2;
+	
+	public static void main(String...args) {
+		System.out.println("Hello Static Blocks!");
+		
+		System.out.printf("value1 = %s\n", mValue1);
+		System.out.printf("value2 = %s\n", mValue2);
+	}
+	
+	static void setValue2(int value) {
+		mValue2 = value;
+	}
+}

5bc5b5a8b5989867a68611a5a4dfa39d59d398f7
John Bannick <jbannick@7128.com>
2017-10-29 10:55:41 -0400

    Update HelloCollectionsSearching.java

--- src/collections/HelloCollectionsSearching.java
+++ src/collections/HelloCollectionsSearching.java
@@ -5,28 +5,25 @@ import java.util.GregorianCalendar;
 import java.util.List;
 
 public class HelloCollectionsSearching {
-	
-	//TODO: EXAMPLE WITH A COMPARATOR
-	//LOOK AT DATA TYPES IN MY SORTING.DOC
-	
-	static Byte[] bytes = {26, 10, 11, 1};
-	static String[] words = {"zzz", "mmm", "qqq", "aaa"};
-	static Calendar[] dates = {
-			new GregorianCalendar(2017, 12, 31), 
-			new GregorianCalendar(2017, 10, 1),
-			new GregorianCalendar(2017, 11, 31),
-			new GregorianCalendar(2017, 1, 1)};
-	
-	public static void main(String...args) {
-		System.out.println("Hello Collections Searching!");
 
-		List<Byte> byteList = Arrays.asList(bytes);
-		Byte byteTarget = Byte.parseByte("10");
-		int byteIndex = Collections.binarySearch(byteList, byteTarget);
-		System.out.printf("byteTarget %s is at index %s in ", byteTarget, byteIndex);
-		System.out.println(byteList);
+    // TODO: EXAMPLE WITH A COMPARATOR
+    // LOOK AT DATA TYPES IN MY SORTING.DOC
 
+    static Byte[] bytes = { 26, 10, 11, 1 };
+    static String[] words = { "zzz", "mmm", "qqq", "aaa" };
+    static Calendar[] dates = { 
+        new GregorianCalendar(2017, 12, 31), new GregorianCalendar(2017, 10, 1),
+        new GregorianCalendar(2017, 11, 31), new GregorianCalendar(2017, 1, 1) };
 
+    public static void main(String... args) {
+        System.out.println("Hello Collections Searching!");
 
-	}
+        List<Byte> byteList = Arrays.asList(bytes);
+        Byte byteTarget = Byte.parseByte("10");
+        int byteIndex = Collections.binarySearch(byteList, byteTarget);
+        System.out.printf("byteTarget %s is at index %s in ", byteTarget, byteIndex);
+        System.out.println(byteList);
+
+        // TODO: Complete this Kata
+    }
 }

d9dbd61bb94a5e7a2713705ec83af19e1be054a5
John Bannick <jbannick@7128.com>
2017-10-29 10:38:29 -0400

    Update HelloAnnotations.java

--- src/annotations/HelloAnnotations.java
+++ src/annotations/HelloAnnotations.java
@@ -2,42 +2,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HelloAnnotations {
-	public static void main(String...args) {
-		System.out.println("Hello Annotations!");
-		
-		HelloAnnotations ha = new HelloAnnotations();
-		ha.myMethod();
-		
-		MyAnnotations ma = new MyAnnotations();
-		ma.myMethod();
-		
-		ha.unCheckedGenerics();
-		ha.unCheckedGenerics2();
-	}
-	
-	public void myMethod() {
-		System.out.println("MyMethod");
-	}
-	
-	@SuppressWarnings("unchecked")
-	public void unCheckedGenerics() {
-		@SuppressWarnings("rawtypes")
-		List animals = new ArrayList();
-		animals.add("cat");
-		System.out.println(animals);
-	}
-	
-	@SuppressWarnings({"unchecked", "rawtypes"})
-	public void unCheckedGenerics2() {
-		List animals = new ArrayList();
-		animals.add("dog");
-		System.out.println(animals);
-	}
+    public static void main(String... args) {
+        System.out.println("Hello Annotations!");
+
+        HelloAnnotations ha = new HelloAnnotations();
+        ha.myMethod();
+
+        MyAnnotations ma = new MyAnnotations();
+        ma.myMethod();
+
+        ha.unCheckedGenerics();
+        ha.unCheckedGenerics2();
+    }
+
+    public void myMethod() {
+        System.out.println("MyMethod");
+    }
+
+    @SuppressWarnings("unchecked")
+    public void unCheckedGenerics() {
+        @SuppressWarnings("rawtypes")
+        List animals = new ArrayList();
+        animals.add("cat");
+        System.out.println(animals);
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public void unCheckedGenerics2() {
+        List animals = new ArrayList();
+        animals.add("dog");
+        System.out.println(animals);
+    }
 }
 
 class MyAnnotations extends HelloAnnotations {
-	@Override
-	public void myMethod() {
-		System.out.println("Overriden MyMethod");
-	}
+    @Override
+    public void myMethod() {
+        System.out.println("Overriden MyMethod");
+    }
 }

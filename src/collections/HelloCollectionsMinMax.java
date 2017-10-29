d65d38bda1d11c3cd164948798059e07b37ed080
John Bannick <jbannick@7128.com>
2017-10-29 10:53:56 -0400

    Update HelloCollectionsMinMax.java

--- src/collections/HelloCollectionsMinMax.java
+++ src/collections/HelloCollectionsMinMax.java
@@ -5,36 +5,33 @@ import java.util.GregorianCalendar;
 import java.util.List;
 
 public class HelloCollectionsMinMax {
-	
-	//LOOK AT DATA TYPES IN MY SORTING.DOC
-	
-	static Byte[] bytes = {26, 10, 11, 1};
-	static String[] words = {"zzz", "mmm", "qqq", "aaa"};
-	static Calendar[] dates = {
-			new GregorianCalendar(2017, 12, 31), 
-			new GregorianCalendar(2017, 10, 1),
-			new GregorianCalendar(2017, 11, 31),
-			new GregorianCalendar(2017, 1, 1)};
-	
-	public static void main(String...args) {
-		System.out.println("Hello Collections Min Max!");
 
-		List<Byte> listBytes = Arrays.asList(bytes);
-		Collections.sort(listBytes);
-		System.out.println(listBytes);
-		
-		System.out.printf("bytes: min = %s, max = %s\n", 
-			Collections.min(listBytes),  Collections.max(listBytes));
+    // LOOK AT DATA TYPES IN MY SORTING.DOC
 
-//		List<String> listStrings = Arrays.asList(words);
-//		Collections.sort(listStrings);
-//		System.out.println(listStrings);
-//
-//		List<Calendar> listDates = Arrays.asList(dates);
-//		Collections.sort(listDates);
-//		for (Calendar date : listDates) {
-//			System.out.println(date.getTime().toString()); // THIS IS BUGGY
-//		}
+    static Byte[] bytes = { 26, 10, 11, 1 };
+    static String[] words = { "zzz", "mmm", "qqq", "aaa" };
+    static Calendar[] dates = { new GregorianCalendar(2017, 12, 31), new GregorianCalendar(2017, 10, 1),
+            new GregorianCalendar(2017, 11, 31), new GregorianCalendar(2017, 1, 1) };
 
-	}
+    public static void main(String... args) {
+        System.out.println("Hello Collections Min Max!");
+
+        List<Byte> listBytes = Arrays.asList(bytes);
+        Collections.sort(listBytes);
+        System.out.println(listBytes);
+
+        System.out.printf("bytes: min = %s, max = %s\n", Collections.min(listBytes), Collections.max(listBytes));
+
+        // List<String> listStrings = Arrays.asList(words);
+        // Collections.sort(listStrings);
+        // System.out.println(listStrings);
+        //
+        // List<Calendar> listDates = Arrays.asList(dates);
+        // Collections.sort(listDates);
+        // for (Calendar date : listDates) {
+        // System.out.println(date.getTime().toString()); // THIS IS BUGGY
+        // }
+
+        // TODO: Complete this Kata
+    }
 }

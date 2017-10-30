b0e0f8745f2db98b89f55b5408581577250da1af
John Bannick <jbannick@7128.com>
2017-10-30 05:08:27 -0400

    Update HelloConsoleIOScanner.java

--- src/consoleio/HelloConsoleIOScanner.java
+++ src/consoleio/HelloConsoleIOScanner.java
@@ -1,24 +1,23 @@
-
 import java.util.Scanner;
 
 public class HelloConsoleIOScanner {
-	public static void main(String...args) {
-		System.out.println("Hello Console IO Scanner!");
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("\nEnter ABC-12345: ");
-		String productId = scanner.next("[A-Z]{3}-[0-9]{5}");
-		System.out.println(productId);
-		
-		System.out.println("\nEnter 123: ");
-		int quantity = scanner.nextInt();
-		System.out.println(quantity);
-		
-		System.out.println("\nEnter 1.23: ");
-		float price = scanner.nextFloat();
-		System.out.println(price);
-		
-		scanner.close();
-		System.out.println("\nExiting");
-	}
+    public static void main(String... args) {
+        System.out.println("Hello Console IO Scanner!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nEnter ABC-12345: ");
+        String productId = scanner.next("[A-Z]{3}-[0-9]{5}");
+        System.out.println(productId);
+
+        System.out.println("\nEnter 123: ");
+        int quantity = scanner.nextInt();
+        System.out.println(quantity);
+
+        System.out.println("\nEnter 1.23: ");
+        float price = scanner.nextFloat();
+        System.out.println(price);
+
+        scanner.close();
+        System.out.println("\nExiting");
+    }
 }

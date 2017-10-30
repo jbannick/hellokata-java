a3313bb27d68fc1faa790c8ad3e65d2916d3577a
John Bannick <jbannick@7128.com>
2017-10-30 16:47:21 -0400

    Update HelloDataTypesBoolean.java

--- src/datatypes/HelloDataTypesBoolean.java
+++ src/datatypes/HelloDataTypesBoolean.java
@@ -1,56 +1,56 @@
 public class HelloDataTypesBoolean {
-	public static void main(String...args) {
-		System.out.println("Hello Data Types Boolean!");
-		
-		// The official size of a Boolean is "not precisely defined" 
-		// But tests suggest that it is one byte.
+    public static void main(String... args) {
+        System.out.println("Hello Data Types Boolean!");
 
-		// 1. Value by Assigning a Literal
-		
-		boolean bo1 = Boolean.FALSE;
-		boolean bo2 = Boolean.TRUE;
-		
-		boolean bo3 = true;
-		Boolean bo4 = true;
-		
-		System.out.printf("boolean bo1 = Boolean.FALSE; \t%s\n", bo1);
-		System.out.printf("boolean bo2 = Boolean.TRUE; \t%s\n", bo2);
-		System.out.printf("boolean bo3 = true; \t\t%s\n", bo3);
-		System.out.printf("Boolean bo4 = true; \t\t%s\n", bo4);
+        // The official size of a Boolean is "not precisely defined"
+        // But tests suggest that it is one byte.
 
-		// 2. Value by Instantiation
-		
-		Boolean bo5 = new Boolean(true);
-		Boolean bo6 = Boolean.valueOf(true);
+        // 1. Value by Assigning a Literal
 
-		System.out.printf("Boolean bo5 = new Boolean(true); \t%s\n", bo5);
-		System.out.printf("Boolean bo6 = Boolean.valueOf(true); \t%s\n", bo6);
+        boolean bo1 = Boolean.FALSE;
+        boolean bo2 = Boolean.TRUE;
 
-		// 3. Value by Explicit Conversion
-		boolean bo7 = bo5.booleanValue();
+        boolean bo3 = true;
+        Boolean bo4 = true;
 
-		System.out.printf("boolean bo7 = bo5.booleanValue(); \t\t%s\n", bo7);
+        System.out.printf("boolean bo1 = Boolean.FALSE; \t%s\n", bo1);
+        System.out.printf("boolean bo2 = Boolean.TRUE; \t%s\n", bo2);
+        System.out.printf("boolean bo3 = true; \t\t%s\n", bo3);
+        System.out.printf("Boolean bo4 = true; \t\t%s\n", bo4);
 
-		// 4. Value by Autoboxing (Implicit Conversion)
-		
-		boolean bo8 = bo5;
-		Boolean bo9 = bo3; 
+        // 2. Value by Instantiation
 
-		System.out.printf("boolean bo8 = bo5; \t\t\t%s\n", bo8);
-		System.out.printf("Boolean bo9 = bo3;  \t\t\t%s\n", bo9);
+        Boolean bo5 = new Boolean(true);
+        Boolean bo6 = Boolean.valueOf(true);
 
-		// 5. Value By Parsing
-		
-		boolean bo10 = Boolean.parseBoolean("true");
-		boolean bo11 = Boolean.valueOf("true");
-		boolean bo12 = Boolean.getBoolean("true");
+        System.out.printf("Boolean bo5 = new Boolean(true); \t%s\n", bo5);
+        System.out.printf("Boolean bo6 = Boolean.valueOf(true); \t%s\n", bo6);
 
-		System.out.printf("boolean bo10 = Boolean.parseBoolean(\"true\");  \t%s\n", bo10);
-		System.out.printf("boolean bo11 = Boolean.valueOf(\"true\");  \t%s\n", bo11);
-		System.out.printf("boolean bo12 = Boolean.getBoolean(\"true\");  \t%s\n", bo12);
+        // 3. Value by Explicit Conversion
+        boolean bo7 = bo5.booleanValue();
 
-		// 6. Value By Casting
-		
-		// You cannot cast from another data type to a boolean.
-	}
+        System.out.printf("boolean bo7 = bo5.booleanValue(); \t\t%s\n", bo7);
+
+        // 4. Value by Autoboxing (Implicit Conversion)
+
+        boolean bo8 = bo5;
+        Boolean bo9 = bo3;
+
+        System.out.printf("boolean bo8 = bo5; \t\t\t%s\n", bo8);
+        System.out.printf("Boolean bo9 = bo3;  \t\t\t%s\n", bo9);
+
+        // 5. Value By Parsing
+
+        boolean bo10 = Boolean.parseBoolean("true");
+        boolean bo11 = Boolean.valueOf("true");
+        boolean bo12 = Boolean.getBoolean("true");
+
+        System.out.printf("boolean bo10 = Boolean.parseBoolean(\"true\");  \t%s\n", bo10);
+        System.out.printf("boolean bo11 = Boolean.valueOf(\"true\");  \t%s\n", bo11);
+        System.out.printf("boolean bo12 = Boolean.getBoolean(\"true\");  \t%s\n", bo12);
+
+        // 6. Value By Casting
+
+        // You cannot cast from another data type to a boolean.
+    }
 }

2a5714a88e8e0cf2b3139262c4eb6c9eb3b58792
John Bannick <jbannick@7128.com>
2017-10-30 05:06:09 -0400

    Update HelloConsoleIOConsole.java

--- src/consoleio/HelloConsoleIOConsole.java
+++ src/consoleio/HelloConsoleIOConsole.java
@@ -3,25 +3,25 @@ import java.io.Console;
 import java.util.Arrays;
 
 public class HelloConsoleIOConsole {
-	public static void main(String...args) {
-		System.out.println("Hello Console IO Console!");
-		
-		Console console = System.console();
-		if (null == console) {
-			System.err.println("Can not instantiate Console");
-			System.exit(-1);
-		}
-		System.out.println("Enter username: ");
-		String username = console.readLine("user: ");
-		System.out.println(username);
-		
-		char[] correctPassword = "swordfish".toCharArray();
-		System.out.println("Enter password: ");
-		char[] password = console.readPassword("password: ");
-		if (Arrays.equals(password, correctPassword)) {
-			System.out.println("Correct!");
-		} else {
-			System.out.println("Fail!");
-		}
-	}
+    public static void main(String... args) {
+        System.out.println("Hello Console IO Console!");
+
+        Console console = System.console();
+        if (null == console) {
+            System.err.println("Can not instantiate Console");
+            System.exit(-1);
+        }
+        System.out.println("Enter username: ");
+        String username = console.readLine("user: ");
+        System.out.println(username);
+
+        char[] correctPassword = "swordfish".toCharArray();
+        System.out.println("Enter password: ");
+        char[] password = console.readPassword("password: ");
+        if (Arrays.equals(password, correctPassword)) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println("Fail!");
+        }
+    }
 }

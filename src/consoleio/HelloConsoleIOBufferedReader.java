c803ec9e80c9986f6f7f532f1dc5c212355e2224
John Bannick <jbannick@7128.com>
2017-10-30 05:05:00 -0400

    Update HelloConsoleIOBufferedReader.java

--- src/consoleio/HelloConsoleIOBufferedReader.java
+++ src/consoleio/HelloConsoleIOBufferedReader.java
@@ -3,16 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class HelloConsoleIOBufferedReader {
-	public static void main(String...args) {
-		System.out.println("Hello Console IO Buffered Reader!");
-		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			System.out.println("Enter something: ");
-			String line = reader.readLine();
-			System.out.println(line);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String... args) {
+        System.out.println("Hello Console IO Buffered Reader!");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.println("Enter something: ");
+            String line = reader.readLine();
+            System.out.println(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

b90a7332ee5e3faf1ab3c591dc8f4981d3c7aabd
John Bannick <jbannick@7128.com>
2017-10-30 05:03:37 -0400

    Update HelloComments.java

--- src/comments/HelloComments.java
+++ src/comments/HelloComments.java
@@ -1,16 +1,16 @@
 
 public class HelloComments {
-	/**
-	 * This is a Javadoc comment.
-	 */
-	public static void main(String...args) {
-		
-		/*
-		 * This is a C Language style Block Implementation comment.
-		 */
-				
-		// This is a Single Line, Implementation comment.
-		
-		System.out.println("Hello Comments!"); 		
-	}
+    /**
+     * This is a Javadoc comment.
+     */
+    public static void main(String... args) {
+
+        /*
+         * This is a C Language style Block Implementation comment.
+         */
+
+        // This is a Single Line, Implementation comment.
+
+        System.out.println("Hello Comments!");
+    }
 }

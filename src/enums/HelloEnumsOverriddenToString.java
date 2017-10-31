7a9118a634fcfc8eea46503fe493fc323e8eb8c4
John Bannick <jbannick@7128.com>
2017-10-31 04:32:21 -0400

    Update HelloEnumsOverriddenToString.java

--- src/enums/HelloEnumsOverriddenToString.java
+++ src/enums/HelloEnumsOverriddenToString.java
@@ -1,28 +1,29 @@
 public class HelloEnumsOverriddenToString {
-	
-	public enum Simplest {A, B, C, D;
-		@Override
-		public String toString() {
-			switch(this) {
-			case A:
-				return "A is for Alligator";
-			case B:
-				return "B is for Beer";
-			case C:
-				return "C is for Catterpiggle";
-			case D:
-				return "D is for Dear";
-			default:
-				return "Unknown";
-			}
-		}
-	};
-	
-	public static void main(String...args) {
-		System.out.println("Hello Enums Overridden toString!");
-		
-		for (Simplest s : Simplest.values()) {
-			System.out.println(s);
-		}
-	}
+
+    public enum Simplest {
+        A, B, C, D;
+        @Override
+        public String toString() {
+            switch (this) {
+            case A:
+                return "A is for Alligator";
+            case B:
+                return "B is for Beer";
+            case C:
+                return "C is for Catterpiggle";
+            case D:
+                return "D is for Dear";
+            default:
+                return "Unknown";
+            }
+        }
+    };
+
+    public static void main(String... args) {
+        System.out.println("Hello Enums Overridden toString!");
+
+        for (Simplest s : Simplest.values()) {
+            System.out.println(s);
+        }
+    }
 }

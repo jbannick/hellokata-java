25d0d99390af8616a019ed9c0f971fcaa3df58ab
John Bannick <jbannick@7128.com>
2017-10-31 04:39:41 -0400

    Update HelloEnumSetSimplest.java

--- src/enumset/HelloEnumSetSimplest.java
+++ src/enumset/HelloEnumSetSimplest.java
@@ -1,13 +1,15 @@
 import java.util.EnumSet;
 
-enum Simple {A, B, C}
+enum Simple {
+    A, B, C
+}
 
 public class HelloEnumSetSimplest {
-	public static void main(String...args) {
-		System.out.println("Hello EnumSet Simplest!");
-		
-		EnumSet<Simple> eSet = EnumSet.of(Simple.A, Simple.B, Simple.C);
-		
-		System.out.println(eSet);		
-	}
+    public static void main(String... args) {
+        System.out.println("Hello EnumSet Simplest!");
+
+        EnumSet<Simple> eSet = EnumSet.of(Simple.A, Simple.B, Simple.C);
+
+        System.out.println(eSet);
+    }
 }

9e37f24c7dd78a8f6dd59fa190ddda91d8644010
John Bannick <jbannick@7128.com>
2017-10-31 04:33:06 -0400

    Update HelloEnumsSimplest.java

--- src/enums/HelloEnumsSimplest.java
+++ src/enums/HelloEnumsSimplest.java
@@ -1,12 +1,14 @@
 public class HelloEnumsSimplest {
-	
-	public enum Simplest {A, B, C}
-	
-	public static void main(String...args) {
-		System.out.println("Hello Enums Simplest!");
-		
-		Simplest simplestA = Simplest.A;
-		System.out.printf("SimplestA = %s\n", simplestA);
-		System.out.printf("Simplest.B = %s\n", Simplest.B);
-	}
+
+    public enum Simplest {
+        A, B, C
+    }
+
+    public static void main(String... args) {
+        System.out.println("Hello Enums Simplest!");
+
+        Simplest simplestA = Simplest.A;
+        System.out.printf("SimplestA = %s\n", simplestA);
+        System.out.printf("Simplest.B = %s\n", Simplest.B);
+    }
 }

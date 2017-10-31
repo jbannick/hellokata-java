2a87481d4ea97b88b468f8d69c72347062e59c25
John Bannick <jbannick@7128.com>
2017-10-31 04:29:50 -0400

    Update HelloEnumMapUseCase.java

--- src/enummap/HelloEnumMapUseCase.java
+++ src/enummap/HelloEnumMapUseCase.java
@@ -1,22 +1,24 @@
 import java.util.EnumMap;
 
 public class HelloEnumMapUseCase {
-	enum Status {OK, WARN, ERROR}
-	
-	public static void main(String...args) {
-		System.out.println("Hello EnumMap Use Case!");
-		
-		EnumMap<Status,Integer> weekdayStatusCounts = new EnumMap<Status,Integer>(Status.class);
-		weekdayStatusCounts.put(Status.OK, 10);
-		weekdayStatusCounts.put(Status.WARN, 20);
-		weekdayStatusCounts.put(Status.ERROR, 3);
+    enum Status {
+        OK, WARN, ERROR
+    }
 
-		EnumMap<Status,Integer> weekendStatusCounts = new EnumMap<Status,Integer>(Status.class);
-		weekendStatusCounts.put(Status.OK, 15);
-		weekendStatusCounts.put(Status.WARN, 22);
-		weekendStatusCounts.put(Status.ERROR, 0);
-		
-		System.out.printf("Weekday: %s\n", weekdayStatusCounts);
-		System.out.printf("Weekend: %s\n", weekendStatusCounts);
-	}
+    public static void main(String... args) {
+        System.out.println("Hello EnumMap Use Case!");
+
+        EnumMap<Status, Integer> weekdayStatusCounts = new EnumMap<Status, Integer>(Status.class);
+        weekdayStatusCounts.put(Status.OK, 10);
+        weekdayStatusCounts.put(Status.WARN, 20);
+        weekdayStatusCounts.put(Status.ERROR, 3);
+
+        EnumMap<Status, Integer> weekendStatusCounts = new EnumMap<Status, Integer>(Status.class);
+        weekendStatusCounts.put(Status.OK, 15);
+        weekendStatusCounts.put(Status.WARN, 22);
+        weekendStatusCounts.put(Status.ERROR, 0);
+
+        System.out.printf("Weekday: %s\n", weekdayStatusCounts);
+        System.out.printf("Weekend: %s\n", weekendStatusCounts);
+    }
 }

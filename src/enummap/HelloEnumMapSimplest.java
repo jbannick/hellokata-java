a2d8515ab7ef9df5115343177741f35400ec3888
John Bannick <jbannick@7128.com>
2017-10-31 04:28:56 -0400

    Update HelloEnumMapSimplest.java

--- src/enummap/HelloEnumMapSimplest.java
+++ src/enummap/HelloEnumMapSimplest.java
@@ -1,17 +1,19 @@
 import java.util.EnumMap;
 
-enum Status {OK, WARN, ERROR}
+enum Status {
+    OK, WARN, ERROR
+}
 
 public class HelloEnumMapSimplest {
-	public static void main(String...args) {
-		System.out.println("Hello EnumMap Simplest!");
-		EnumMap<Status,Integer> statusCounts = new EnumMap<Status,Integer>(Status.class);
-		statusCounts.put(Status.OK, 10);
-		statusCounts.put(Status.WARN, 20);
-		statusCounts.put(Status.ERROR, 3);
-		int countErrors = statusCounts.get(Status.ERROR);
-		
-		System.out.printf("Status counts: %s\n", statusCounts);
-		System.out.printf("countErrors = %s\n", countErrors);
-	}
+    public static void main(String... args) {
+        System.out.println("Hello EnumMap Simplest!");
+        EnumMap<Status, Integer> statusCounts = new EnumMap<Status, Integer>(Status.class);
+        statusCounts.put(Status.OK, 10);
+        statusCounts.put(Status.WARN, 20);
+        statusCounts.put(Status.ERROR, 3);
+        int countErrors = statusCounts.get(Status.ERROR);
+
+        System.out.printf("Status counts: %s\n", statusCounts);
+        System.out.printf("countErrors = %s\n", countErrors);
+    }
 }

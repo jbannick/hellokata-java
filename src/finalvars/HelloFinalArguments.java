b2a444bb45eef6565915cb310e649d5fbfb27959
John Bannick <jbannick@7128.com>
2017-10-31 04:57:56 -0400

    Update HelloFinalArguments.java

--- src/finalvars/HelloFinalArguments.java
+++ src/finalvars/HelloFinalArguments.java
@@ -1,25 +1,24 @@
-
 public class HelloFinalArguments {
-	public static void main(String...args) {
-		System.out.println("Hello Final Arguments!");
-		Circle circle = new Circle(3.0d);
-		System.out.printf("circle: radius = %s\n", circle.getRadius());
-	}
+    public static void main(String... args) {
+        System.out.println("Hello Final Arguments!");
+        Circle circle = new Circle(3.0d);
+        System.out.printf("circle: radius = %s\n", circle.getRadius());
+    }
 }
 
 class Circle3 {
-	private double radius;
-	
-	Circle3(final double radius) {
-		this.radius = radius;
-		
-		// Bad Code
-		// The final local variable radius cannot be assigned. 
-		// It must be blank and not using a compound assignment
-//		radius = 7d; 
-	}
-	
-	double getRadius() {
-		return this.radius;
-	}
+    private double radius;
+
+    Circle3(final double radius) {
+        this.radius = radius;
+
+        // Bad Code
+        // The final local variable radius cannot be assigned.
+        // It must be blank and not using a compound assignment
+        // radius = 7d;
+    }
+
+    double getRadius() {
+        return this.radius;
+    }
 }

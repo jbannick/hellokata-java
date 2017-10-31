d40c8765886e83babea9871851953155904f6f10
John Bannick <jbannick@7128.com>
2017-10-31 04:52:47 -0400

    Update HelloDigestInputAndOutputStreams.java

--- src/fileiostreams/HelloDigestInputAndOutputStreams.java
+++ src/fileiostreams/HelloDigestInputAndOutputStreams.java
@@ -9,49 +9,48 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class HelloDigestInputAndOutputStreams {
-	
-	private static final String FILENAME = "md5.txt";
-	public static void main(String...args) {
-		System.out.println("Hello Digest Input and Output Streams");
-		
-		try (DigestOutputStream dos = new DigestOutputStream(
-			new FileOutputStream(FILENAME),
-			MessageDigest.getInstance("md5"))) {
-						
-			for (int i = 0; i < 100; i++)  {
-				dos.write("Hello World\n".getBytes());
-			}
-			
-			MessageDigest md = dos.getMessageDigest();
-			String digestStr = new BigInteger(1, md.digest()).toString(16);
-			System.out.printf("Message Digest after writing: %s\n", digestStr);
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-				
-		try (DigestInputStream dis = new DigestInputStream(
-			new FileInputStream(FILENAME),
-			MessageDigest.getInstance("md5"))) {
-			
-			while (dis.read() != -1) {
-				// null-op
-			}
-			
-			MessageDigest md = dis.getMessageDigest();
-			String digestStr = new BigInteger(1, md.digest()).toString(16);
-			System.out.printf("Message Digest after reading: %s\n", digestStr);
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    private static final String FILENAME = "md5.txt";
+
+    public static void main(String... args) {
+        System.out.println("Hello Digest Input and Output Streams");
+
+        try (DigestOutputStream dos = new DigestOutputStream(new FileOutputStream(FILENAME),
+                MessageDigest.getInstance("md5"))) {
+
+            for (int i = 0; i < 100; i++) {
+                dos.write("Hello World\n".getBytes());
+            }
+
+            MessageDigest md = dos.getMessageDigest();
+            String digestStr = new BigInteger(1, md.digest()).toString(16);
+            System.out.printf("Message Digest after writing: %s\n", digestStr);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (DigestInputStream dis = new DigestInputStream(new FileInputStream(FILENAME),
+                MessageDigest.getInstance("md5"))) {
+
+            while (dis.read() != -1) {
+                // null-op
+            }
+
+            MessageDigest md = dis.getMessageDigest();
+            String digestStr = new BigInteger(1, md.digest()).toString(16);
+            System.out.printf("Message Digest after reading: %s\n", digestStr);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

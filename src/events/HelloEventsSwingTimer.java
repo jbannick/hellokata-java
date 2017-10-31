87028d35c3841cd973822d48fbdf378f4b7d1028
John Bannick <jbannick@7128.com>
2017-10-31 04:42:26 -0400

    Update HelloEventsSwingTimer.java

--- src/events/HelloEventsSwingTimer.java
+++ src/events/HelloEventsSwingTimer.java
@@ -3,26 +3,27 @@ import java.awt.event.ActionListener;
 import javax.swing.Timer;
 
 public class HelloEventsSwingTimer {
-	public final static int ONE_SECOND = 1000;
-	static int fCount = 0;
-	static Timer fTimer = null;
-	public static void main(String...args) {
-		System.out.println("Hello Events Swing Timer!");
-		
-		fTimer = new Timer(ONE_SECOND, new ActionListener() {
+    public final static int ONE_SECOND = 1000;
+    static int fCount = 0;
+    static Timer fTimer = null;
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.out.printf("Tick %s\n", fCount);
-				fCount++;
-				if (fCount > 5) {
-					fTimer.stop();
-					System.out.println("I'm stopped");
-				}
-			}
+    public static void main(String... args) {
+        System.out.println("Hello Events Swing Timer!");
 
-		});
-		
-		fTimer.start();
-	}
+        fTimer = new Timer(ONE_SECOND, new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.printf("Tick %s\n", fCount);
+                fCount++;
+                if (fCount > 5) {
+                    fTimer.stop();
+                    System.out.println("I'm stopped");
+                }
+            }
+
+        });
+
+        fTimer.start();
+    }
 }

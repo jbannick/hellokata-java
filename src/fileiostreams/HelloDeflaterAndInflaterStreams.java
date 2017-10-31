d1c6d89904f7b707a3adf725ba070f38c172cb3b
John Bannick <jbannick@7128.com>
2017-10-31 04:51:56 -0400

    Update HelloDeflaterAndInflaterStreams.java

--- src/fileiostreams/HelloDeflaterAndInflaterStreams.java
+++ src/fileiostreams/HelloDeflaterAndInflaterStreams.java
@@ -1,4 +1,3 @@
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -8,40 +7,37 @@ import java.util.zip.DeflaterInputStream;
 import java.util.zip.DeflaterOutputStream;
 
 public class HelloDeflaterAndInflaterStreams {
-	private static final String FILENAME = "deflated.txt";
-	public static void main(String...args) {
-		System.out.println("Hello Deflater and Inflater Streams");
-		
-		try {
-			DeflaterOutputStream dos = new DeflaterOutputStream(
-				new FileOutputStream(
-					new File(FILENAME)));
-			
-			for (int i = 0; i < 100; i++)  {
-				dos.write("Hello World\n".getBytes());
-			}
-			dos.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    private static final String FILENAME = "deflated.txt";
 
-		
-		try {
-			DeflaterInputStream dis = new DeflaterInputStream(
-				new FileInputStream(
-					new File(FILENAME)));
-			
-			for (int i = 0; i < 100; i++)  {
-				System.out.print(dis.read());
-			}
-			dis.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    public static void main(String... args) {
+        System.out.println("Hello Deflater and Inflater Streams");
 
-	}
+        try {
+            DeflaterOutputStream dos = 
+                new DeflaterOutputStream(new FileOutputStream(new File(FILENAME)));
+
+            for (int i = 0; i < 100; i++) {
+                dos.write("Hello World\n".getBytes());
+            }
+            dos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            DeflaterInputStream dis = 
+                new DeflaterInputStream(new FileInputStream(new File(FILENAME)));
+
+            for (int i = 0; i < 100; i++) {
+                System.out.print(dis.read());
+            }
+            dis.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

2e29811e96c0a50722dcce507a40799d047a65f7
John Bannick <jbannick@7128.com>
2017-10-31 04:28:26 -0400

    Update HelloEnumMapIteration.java

--- src/enummap/HelloEnumMapIteration.java
+++ src/enummap/HelloEnumMapIteration.java
@@ -3,40 +3,42 @@ import java.util.Iterator;
 import java.util.Map.Entry;
 
 public class HelloEnumMapIteration {
-	enum Status {OK, WARN, ERROR}
+    enum Status {
+        OK, WARN, ERROR
+    }
 
-	public static void main(String...args) {
-		System.out.println("Hello EnumMap Iteration!");
-		
-		EnumMap<Status,Integer> statusCounts = new EnumMap<Status,Integer>(Status.class);
-		statusCounts.put(Status.OK, 10);
-		statusCounts.put(Status.WARN, 20);
-		statusCounts.put(Status.ERROR, 3);
+    public static void main(String... args) {
+        System.out.println("Hello EnumMap Iteration!");
 
-		System.out.println("Iterator<Status> it = statusCounts.keySet().iterator(); while (it.hasNext()) {...");
-		Iterator<Status> it = statusCounts.keySet().iterator();
-		while (it.hasNext()) {
-			Status  key   = it.next();
-			Integer value = statusCounts.get(key);
-			System.out.printf("%s %s\n", key, value);
-		}
-		
-		System.out.println("for (Status s : statusCounts.keySet()) {...");
-		for (Status s : statusCounts.keySet()) {
-			System.out.printf("%s %s\n", s, statusCounts.get(s));
-		}
-		
-		System.out.println("for (Entry<Status,Integer> entry : statusCounts.entrySet()) {...");
-		for (Entry<Status,Integer> entry : statusCounts.entrySet()) {
-			System.out.printf("%s\n", entry);
-		}
-	
-		System.out.println("statusCounts.forEach((key, value) -> {...");
-		statusCounts.forEach((key, value) -> {
-			System.out.printf("%s %s\n", key, value);
-		});
-		
-		System.out.println("statusCounts.entrySet().stream().forEach(System.out::println);");
-		statusCounts.entrySet().stream().forEach(System.out::println);
-	}
+        EnumMap<Status, Integer> statusCounts = new EnumMap<Status, Integer>(Status.class);
+        statusCounts.put(Status.OK, 10);
+        statusCounts.put(Status.WARN, 20);
+        statusCounts.put(Status.ERROR, 3);
+
+        System.out.println("Iterator<Status> it = statusCounts.keySet().iterator(); while (it.hasNext()) {...");
+        Iterator<Status> it = statusCounts.keySet().iterator();
+        while (it.hasNext()) {
+            Status key = it.next();
+            Integer value = statusCounts.get(key);
+            System.out.printf("%s %s\n", key, value);
+        }
+
+        System.out.println("for (Status s : statusCounts.keySet()) {...");
+        for (Status s : statusCounts.keySet()) {
+            System.out.printf("%s %s\n", s, statusCounts.get(s));
+        }
+
+        System.out.println("for (Entry<Status,Integer> entry : statusCounts.entrySet()) {...");
+        for (Entry<Status, Integer> entry : statusCounts.entrySet()) {
+            System.out.printf("%s\n", entry);
+        }
+
+        System.out.println("statusCounts.forEach((key, value) -> {...");
+        statusCounts.forEach((key, value) -> {
+            System.out.printf("%s %s\n", key, value);
+        });
+
+        System.out.println("statusCounts.entrySet().stream().forEach(System.out::println);");
+        statusCounts.entrySet().stream().forEach(System.out::println);
+    }
 }

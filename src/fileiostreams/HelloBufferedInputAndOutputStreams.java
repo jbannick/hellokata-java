fbec8c580d1657b7d960313204c94484714f1e6d
John Bannick <jbannick@7128.com>
2017-10-31 04:46:08 -0400

    Update HelloBufferedInputAndOutputStreams.java

--- src/fileiostreams/HelloBufferedInputAndOutputStreams.java
+++ src/fileiostreams/HelloBufferedInputAndOutputStreams.java
@@ -7,42 +7,34 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class HelloBufferedInputAndOutputStreams {
-	private static String FILENAME = "test.txt";
-	private static final int LINECOUNT = 5;
-	
-	public static void main(String...args) {
-		System.out.println("Hello Buffered Input and Output Streams!");
-		
-		try (			
-			BufferedOutputStream bos = new BufferedOutputStream(
-				new FileOutputStream(
-						new File(FILENAME)));
-			) {
-			
-			for (int i = 0; i < LINECOUNT; i++)  {
-				bos.write("Hello World\n".getBytes());
-			}
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		try (			
-			BufferedInputStream bis = new BufferedInputStream(
-				new FileInputStream(
-						new File(FILENAME)));
-			) {
-			
-			while (bis.available() != 0) {
-				System.out.print((char)bis.read());
-			}
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    private static String FILENAME = "test.txt";
+    private static final int LINECOUNT = 5;
+
+    public static void main(String... args) {
+        System.out.println("Hello Buffered Input and Output Streams!");
+
+        try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(new File(FILENAME)));) {
+
+            for (int i = 0; i < LINECOUNT; i++) {
+                bos.write("Hello World\n".getBytes());
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(new File(FILENAME)));) {
+
+            while (bis.available() != 0) {
+                System.out.print((char) bis.read());
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

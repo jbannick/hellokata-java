a7cd6238bc17f88dd4e7e94e08e698d8755f27cc
John Bannick <jbannick@7128.com>
2017-10-31 04:35:18 -0400

    Update HelloEnumsWithInterface.java

--- src/enums/HelloEnumsWithInterface.java
+++ src/enums/HelloEnumsWithInterface.java
@@ -1,31 +1,30 @@
 public class HelloEnumsWithInterface {
-	
-	public interface Price {
-		public double price();
-	}
 
-	public enum Books implements Price {
-		WAR_AND_PEACE (25.99),
-		CATCH_22 (15.99);
-		
-		private final double price;
-		
-		Books(double price) {
-			this.price = price;
-		}
-		
-		@Override
-		public double price() {
-			return this.price;
-		}
-	}
-	
-	public static void main(String...args) {
-		System.out.println("Hello Enums with Interface!");
-		
-		Books  book  = Books.CATCH_22;
-		double price = book.price();
-		
-		System.out.printf("Book = %s, price = %s\n", book, price);
-	}
+    public interface Price {
+        public double price();
+    }
+
+    public enum Books implements Price {
+        WAR_AND_PEACE(25.99), CATCH_22(15.99);
+
+        private final double price;
+
+        Books(double price) {
+            this.price = price;
+        }
+
+        @Override
+        public double price() {
+            return this.price;
+        }
+    }
+
+    public static void main(String... args) {
+        System.out.println("Hello Enums with Interface!");
+
+        Books book = Books.CATCH_22;
+        double price = book.price();
+
+        System.out.printf("Book = %s, price = %s\n", book, price);
+    }
 }

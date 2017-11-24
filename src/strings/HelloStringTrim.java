bbebd244a44e0e9eaa644c302957be4e9030ed7c
John Bannick <jbannick@7128.com>
2017-11-24 15:11:17 -0500

    Update HelloStringTrim.java

--- src/strings/HelloStringTrim.java
+++ src/strings/HelloStringTrim.java
@@ -1,1 +1,11 @@
 
+public class HelloStringTrim {
+
+    public static void main(String... args) {
+        System.out.println("Hello String Trim!");
+
+        String str01 = "\n\t   Hello World!   \t\n".trim();
+
+        System.out.printf("\"\\n\\t   Hello World!   \\t\\n\".trim(); = <%s>\n", str01);
+    }
+}

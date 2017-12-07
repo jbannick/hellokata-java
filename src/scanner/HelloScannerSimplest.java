a3b5c5aa20a065d9c0f08441c7e5ff4091077455
John Bannick <jbannick@7128.com>
2017-12-07 05:07:57 -0500

    Update HelloScannerSimplest.java

--- src/scanner/HelloScannerSimplest.java
+++ src/scanner/HelloScannerSimplest.java
@@ -4,13 +4,18 @@ public class HelloScannerSimplest {
     public static void main(String... args) {
         System.out.println("Hello Scanner Simplest!");
 
-        Scanner sc = new Scanner("Scanner 1 2.0 true");
+        String string = "Scanner 1 2.0 true";
+        
+        Scanner sc = new Scanner(string);
         String st01  = sc.next();
         int in01     = sc.nextInt();
         double db01  = sc.nextDouble();
         boolean bo01 = sc.nextBoolean();
         sc.close();
 
+        System.out.println("---");
+        System.out.println(string);
+        System.out.println("---");
         System.out.printf("sc.next(); = <%s>\n", st01);
         System.out.printf("sc.nextInt(); = %s\n", in01);
         System.out.printf("sc.nextDouble(); = %s\n", db01);

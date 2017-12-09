8c0e97e9639916ba46f462e7df5e232579a25f97
John Bannick <jbannick@7128.com>
2017-12-09 16:15:23 -0500

    Update HelloScannerHas.java

--- src/scanner/HelloScannerHas.java
+++ src/scanner/HelloScannerHas.java
@@ -1,130 +1,93 @@
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.math.RoundingMode;
-import java.util.Random;
 import java.util.Scanner;
-import java.util.StringJoiner;
-import java.util.regex.Pattern;
 
-public class HelloScannerHas {
-
-    static Scanner m_sc;
+public class HelloScannerDelimiters {
 
     public static void main(String... args) {
-        System.out.println("Hello Scanner Has!");
+        System.out.println("Hello Scanner Delimiters!");
 
-        StringJoiner sj = new StringJoiner(" ");
-        sj.add("Scanner");
-
-        sj.add(new BigDecimal(10.0d * 0.0825d).setScale(2, RoundingMode.UP).toPlainString());
-        sj.add(new BigInteger(64, new Random()).toString());
-        sj.add(Boolean.TRUE.toString());
-        sj.add(Byte.toString(Byte.MAX_VALUE));
-        sj.add(Double.toString(Double.MAX_VALUE));
-        sj.add(Float.toString(Float.MAX_VALUE));
-        sj.add(Integer.toString(Integer.MAX_VALUE));
-        sj.add(Long.toString(Long.MAX_VALUE));
-        sj.add(Short.toString(Short.MAX_VALUE));
-        sj.add("Ozymandias");
-
-        System.out.println("---");
-        System.out.println(sj.toString());
+        String string01 = "ABC DEF GHI";
+        String string02 = "123,456,789";
+        String string03 = "086-38-5955 078-05-1120";
+        String string04 = "3.14 1.23 127.0.0.1 172.16.254.1";
 
         // ---
 
-        System.out.println("---");
-        m_sc = new Scanner(sj.toString());
-        whatsNext();
-        String st01 = m_sc.next();
-        System.out.printf("m_sc.next(); = <%s>\n", st01);
+        Scanner sc01 = new Scanner(string01);
+        String st01 = sc01.next();
+        String st02 = sc01.next();
+        String st03 = sc01.next();
+        sc01.close();
 
         System.out.println("---");
-        whatsNext();
-        BigDecimal bd01 = m_sc.nextBigDecimal();
-        System.out.printf("m_sc.nextBigDecimal(); = %s\n", bd01);
+        System.out.println(string01);
+        System.out.printf("sc01.next() = <%s>\n", st01);
+        System.out.printf("sc01.next() = <%s>\n", st02);
+        System.out.printf("sc01.next() = <%s>\n", st03);
+
+        // ---
+
+        Scanner sc02 = new Scanner(string02);
+        sc02.useDelimiter(",");
+        int in11 = sc02.nextInt();
+        int in12 = sc02.nextInt();
+        int in13 = sc02.nextInt();
+        sc02.close();
 
         System.out.println("---");
-        whatsNext();
-        BigInteger bi01 = m_sc.nextBigInteger();
-        System.out.printf("m_sc.nextBigInteger(); = %s\n", bi01);
+        System.out.println(string02);
+        System.out.printf("sc02.nextInt() = %d\n", in11);
+        System.out.printf("sc02.nextInt() = %d\n", in12);
+        System.out.printf("sc02.nextInt() = %d\n", in13);
 
-        System.out.println("---");
-        whatsNext();
-        boolean bo01 = m_sc.nextBoolean();
-        System.out.printf("m_sc.nextBoolean(); = %b\n", bo01);
+        // ---
 
-        System.out.println("---");
-        whatsNext();
-        byte by01 = m_sc.nextByte();
-        System.out.printf("m_sc.nextByte(); = %d\n", by01);
-
-        System.out.println("---");
-        whatsNext();
-        double db01 = m_sc.nextDouble();
-        System.out.printf("m_sc.nextDouble(); = %e\n", db01);
-
-        System.out.println("---");
-        whatsNext();
-        float fl01 = m_sc.nextFloat();
-        System.out.printf("m_sc.nextFloat(); = %e\n", fl01);
-
-        System.out.println("---");
-        whatsNext();
-        int in01 = m_sc.nextInt();
-        System.out.printf("m_sc.nextInt(); = %d\n", in01);
-
-        System.out.println("---");
-        whatsNext();
-        long lo01 = m_sc.nextLong();
-        System.out.printf("m_sc.nextLong(); = %d\n", lo01);
-
-        System.out.println("---");
-        whatsNext();
-        short sh01 = m_sc.nextShort();
-        System.out.printf("m_sc.nextShort(); = %d\n", sh01);
-
-        System.out.println("---");
-        whatsNext();
-        String st02 = m_sc.next(Pattern.compile("Ozy.*"));
-        System.out.printf("m_sc.next(Pattern.compile(\"Ozy.*\")); = <%s>\n", st02);
+        Scanner sc03 = new Scanner(string03);
+        String st21 = sc03.next();
+        sc03.useDelimiter("\\s|\\-");
+        String st22 = sc03.next();
+        String st23 = sc03.next();
+        String st24 = sc03.next();
+        sc03.close();
         
-        m_sc.close();
-    }
+        System.out.println("---");
+        System.out.println(string03);
+        System.out.printf("sc03.next(); = <%s>\n", st21);
+        System.out.printf("sc03.next(); = <%s>\n", st22);
+        System.out.printf("sc03.next(); = <%s>\n", st23);
+        System.out.printf("sc03.next(); = <%s>\n", st24);
 
-    static void whatsNext() {
+        // ---
 
-        StringJoiner sj = new StringJoiner(", ");
+        Scanner sc04 = new Scanner(string04);
+        float fl31 = sc04.nextFloat();
+        System.out.println("---");
+        System.out.println(string04);
+        System.out.printf("sc04.nextFloat(); = %1.2f\n", fl31);
 
-        if (m_sc.hasNextBigDecimal()) {
-            sj.add("BigDecimal");
-        }
-        if (m_sc.hasNextBigInteger()) {
-            sj.add("BigInteger");
-        }
-        if (m_sc.hasNextBoolean()) {
-            sj.add("Boolean");
-        }
-        if (m_sc.hasNextByte()) {
-            sj.add("Byte");
-        }
-        if (m_sc.hasNextDouble()) {
-            sj.add("Double");
-        }
-        if (m_sc.hasNextFloat()) {
-            sj.add("Float");
-        }
-        if (m_sc.hasNextInt()) {
-            sj.add("Integer");
-        }
-        if (m_sc.hasNextLong()) {
-            sj.add("Long");
-        }
-        if (m_sc.hasNextShort()) {
-            sj.add("Short");
-        }
-        if (m_sc.hasNext()) {
-            sj.add("String");
-        }
-        System.out.printf("Next token is one of: %s\n", sj.toString());
+        sc04.useDelimiter("\\s|\\.");
+        int in31 = sc04.nextInt();
+        int in32 = sc04.nextInt();
+        
+        System.out.println("---");
+        System.out.printf("sc04.nextInt(); = %d\n", in31);
+        System.out.printf("sc04.nextInt(); = %d\n", in32);
+
+        sc04.reset();
+        String st31 = sc04.next();
+        System.out.println("---");
+        System.out.printf("sc04.next(); = <%s>\n", st31);
+
+        sc04.useDelimiter("\\s|\\.");
+        int in33 = sc04.nextInt();
+        int in34 = sc04.nextInt();
+        int in35 = sc04.nextInt();
+        int in36 = sc04.nextInt();
+        sc04.close();
+
+        System.out.println("---");
+        System.out.printf("sc04.nextInt(); = %d\n", in33);
+        System.out.printf("sc04.nextInt(); = %d\n", in34);
+        System.out.printf("sc04.nextInt(); = %d\n", in35);
+        System.out.printf("sc04.nextInt(); = %d\n", in36);
     }
 }

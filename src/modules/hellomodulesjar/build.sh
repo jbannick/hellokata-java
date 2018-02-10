e79dbbef467d999604a3106c98d106abf13eb571
John Bannick <jbannick@7128.com>
2018-02-10 11:28:55 -0500

    Delete build.sh

--- src/modules/hellomodulesjar/build.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p out
-mkdir -p mods
-
-javac -d out/hmjar \
-    src/com/kata/hmjar/HelloModulesJar.java \
-    src/module-info.java
-
-jar -cfe mods/hmjar.jar com.kata.hmjar.HelloModulesJar -C out/hmjar .

a5089cd347ccf358022e3dd3f0ba9eb98a5e0bd1
John Bannick <jbannick@7128.com>
2018-03-02 05:24:06 -0500

    Update build.sh

--- src/modules/hellomoduleswin64image/build.sh
+++ src/modules/hellomoduleswin64image/build.sh
@@ -8,7 +8,7 @@ javac -d out \
 jar -cfe mods/hmwin64img.jar kata.modules.hmwin64img.HelloModulesWin64Image -C out .
 
 $JAVA_HOME/bin/jlink \
---module-path mods/:/Users/johnbannick/Documents/java/jdk-win64-9.0.4/jmods \
+--module-path mods/:$JAVA_HOME_WIN64/jmods \
 --add-modules hellomoduleswin64image \
 --launcher hellomoduleswin64image=hellomoduleswin64image \
 --output hellomoduleswin64image-image

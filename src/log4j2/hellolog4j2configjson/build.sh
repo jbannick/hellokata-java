a221a58624d59685bfe619dad3d2891b39006614
John Bannick <jbannick@7128.com>
2018-03-16 07:04:50 -0400

    Create build.sh

---
+++ src/log4j2/hellolog4j2configjson/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+mkdir -p out
+
+javac -d out \
+    -cp ./src:$LOG4J2_HOME/log4j-core-2.10.0.jar:$LOG4J2_HOME/log4j-api-2.10.0.jar:$JSON_HOME/jackson-core-2.9.4.jar:$JSON_HOME/jackson-databind-2.9.4.jar:$JSON_HOME/jackson-annotations-2.9.4.jar \
+    src/HelloLog4J2ConfigJSON.java
+
+cp ./src/log4j2.json ./out

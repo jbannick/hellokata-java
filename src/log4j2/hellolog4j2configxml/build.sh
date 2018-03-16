6979a1c479e920d955dbd8860b6be9d037c94f49
John Bannick <jbannick@7128.com>
2018-03-16 05:05:31 -0400

    Create build.sh

---
+++ src/log4j2/hellolog4j2configxml/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+mkdir -p out
+
+javac -d out \
+    -cp ./src:$LOG4J2_HOME/log4j-core-2.10.0.jar:$LOG4J2_HOME/log4j-api-2.10.0.jar \
+    src/HelloLog4J2ConfigXML.java
+
+cp ./src/log4j2.xml ./out

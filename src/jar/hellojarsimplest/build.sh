3cb82105a3cf07d7250f28f93cc580b25c1979c3
John Bannick <jbannick@7128.com>
2018-03-19 05:19:42 -0400

    Update build.sh

--- src/jar/hellojarsimplest/build.sh
+++ src/jar/hellojarsimplest/build.sh
@@ -3,4 +3,4 @@ mkdir -p out
 
 javac -d out src/org/fubar/app/HelloJARSimplest.java
 
-jar cvfe hellojarsimplest.jar org.fubar.app.HelloJARSimplest -C out .
+jar -cvfe hellojarsimplest.jar org.fubar.app.HelloJARSimplest -C out .

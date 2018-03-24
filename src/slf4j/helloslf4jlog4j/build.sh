5d380a77d9333387feed6467d83d129b7c947514
John Bannick <jbannick@7128.com>
2018-03-24 14:48:25 -0400

    Delete build.sh

--- src/slf4j/helloslf4jlog4j/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p out
-
-javac -d out \
-    -cp ./src:$SLF4J_HOME/slf4j-api-1.8.0-beta2.jar:$SLF4J_HOME/slf4j-simple-1.8.0-beta2.jar:$LOG4J1_HOME/apache-log4j-1.2.jar \
-    src/HelloSLF4JLog4J.java
-
-cp ./src/log4j.properties ./out

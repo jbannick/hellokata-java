0a0b1b781c0021355f13f6b662a861c784d88ace
John Bannick <jbannick@7128.com>
2018-03-24 15:25:09 -0400

    Create HelloLog4j2ConfigJSON.java

---
+++ src/log4j2/hellolog4j2configjson/src/HelloLog4j2ConfigJSON.java
@@ -0,0 +1,13 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class HelloLog4J2ConfigJSON {
+    static final Logger logger = LogManager.getLogger();
+    public static void main(String... args) {
+        System.out.println("Hello Log4J2 Config JSON!");
+        logger.info("This is an info entry");
+        logger.debug("This is a debug entry");
+        logger.warn("This is a warn entry");
+        logger.error("This is an error entry");
+    }
+}

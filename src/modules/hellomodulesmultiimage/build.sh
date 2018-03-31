a4f5805b4da91cb34e5a68089fce09542d7d82a5
John Bannick <jbannick@7128.com>
2018-03-31 14:50:07 -0400

    Update build.sh

--- src/modules/hellomodulesmultiimage/build.sh
+++ src/modules/hellomodulesmultiimage/build.sh
@@ -9,19 +9,19 @@ javac -d out \
 jar -cfe mods/hmod.jar kata.modules.hmod.HelloModules -C out .
 
 $JAVA_HOME/bin/jlink \
---module-path mods/:$JAVA9_MACOS_HOME/jmods \
+--module-path mods/:$JDK_MACOS_HOME/jmods \
 --add-modules hmod \
 --launcher hmod=hmod \
---output hmod-image-java9-macos
+--output hmod-image-java-macos
 
 $JAVA_HOME/bin/jlink \
---module-path mods/:$JAVA9_WIN64_HOME/jmods \
+--module-path mods/:$JDK_WIN64_HOME/jmods \
 --add-modules hmod \
 --launcher hmod=hmod \
---output hmod-image-java9-win64
+--output hmod-image-java-win64
 
 $JAVA_HOME/bin/jlink \
---module-path mods/:$JAVA9_LINUX_HOME/jmods \
+--module-path mods/:$JDK_LINUX_HOME/jmods \
 --add-modules hmod \
 --launcher hmod=hmod \
---output hmod-image-java9-linux
+--output hmod-image-java-linux

d24fa8ff0d09e9cfb10f8c09b863d104d891f747
John Bannick <jbannick@7128.com>
2018-03-31 15:24:32 -0400

    Create HelloModules.java

---
+++ src/modules/hellomodules2/kata/modules/hmod/HelloModules.java
@@ -0,0 +1,11 @@
+package kata.modules.hmod;
+
+import kata.modules.hmod.foo.Foo;
+
+public class HelloModules {
+    public static void main(String... args) {
+        System.out.println("Hello Modules!");
+
+        Foo foo = new Foo();
+    }
+}

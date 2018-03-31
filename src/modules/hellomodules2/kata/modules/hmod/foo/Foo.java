465996b98601d771f7b9ae870ebaafc0f1384812
John Bannick <jbannick@7128.com>
2018-03-31 15:25:08 -0400

    Create Foo.java

---
+++ src/modules/hellomodules2/kata/modules/hmod/foo/Foo.java
@@ -0,0 +1,11 @@
+package kata.modules.hmod.foo;
+
+import org.fubar.Bar;
+
+public class Foo {
+    public Foo() {
+        System.out.println("Foo here!");
+
+        Bar bar = new Bar();
+    }
+}

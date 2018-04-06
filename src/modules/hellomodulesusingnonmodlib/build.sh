85ca17b7ca39ed11a9c3e471ed19ba0786a0c41f
John Bannick <jbannick@7128.com>
2018-04-06 05:06:39 -0400

    Update build.sh

--- src/modules/hellomodulesusingnonmodlib/build.sh
+++ src/modules/hellomodulesusingnonmodlib/build.sh
@@ -3,7 +3,6 @@ mkdir -p out
 mkdir -p mods
 
 cp $EBUS_HOME/eventbus-1.4.jar mods
-CP=./mods/eventbus-1.4.jar
 
 javac --module-path mods -d out --module-source-path src -m appmod\
     src/appmod/kata/app/HelloModulesUsingNonModLib.java \

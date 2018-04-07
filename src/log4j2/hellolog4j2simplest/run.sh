33692435277a7833b2f2e7185f14a500e042bbb7
John Bannick <jbannick@7128.com>
2018-04-07 17:24:51 -0400

    Update run.sh

--- src/log4j2/hellolog4j2simplest/run.sh
+++ src/log4j2/hellolog4j2simplest/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-java -cp ./out:$LOG4J2_HOME/log4j-core-2.10.0.jar:$LOG4J2_HOME/log4j-api-2.10.0.jar \
+java -cp ./out:$LOG4J2_HOME/log4j-core-2.11.0.jar:$LOG4J2_HOME/log4j-api-2.11.0.jar \
     HelloLog4J2Simplest

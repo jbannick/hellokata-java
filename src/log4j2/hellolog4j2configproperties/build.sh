a4c7731a37ec93375acb9c3f3bc99dc5f1ca482a
John Bannick <jbannick@7128.com>
2018-04-07 17:43:17 -0400

    Update build.sh

--- src/log4j2/hellolog4j2configproperties/build.sh
+++ src/log4j2/hellolog4j2configproperties/build.sh
@@ -2,7 +2,7 @@
 mkdir -p out
 
 javac -d out \
-    -cp ./src:$LOG4J2_HOME/log4j-core-2.10.0.jar:$LOG4J2_HOME/log4j-api-2.10.0.jar \
+    -cp ./src:$LOG4J2_HOME/log4j-core-2.11.0.jar:$LOG4J2_HOME/log4j-api-2.11.0.jar \
     src/HelloLog4J2ConfigProperties.java
 
 cp ./src/log4j2.properties ./out

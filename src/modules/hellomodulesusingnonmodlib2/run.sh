e7843605834404951d4f36b50972d89d598b5be8
John Bannick <jbannick@7128.com>
2018-04-07 15:40:08 -0400

    Create run.sh

---
+++ src/modules/hellomodulesusingnonmodlib2/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+CP=lib/jackson-annotations-2.9.0.jar:
+CP+=lib/jackson-core-2.9.0.jar
+
+java -cp $CP --module-path mods:out \
+--module appmod/kata.app.HelloModulesUsingNonModLib2

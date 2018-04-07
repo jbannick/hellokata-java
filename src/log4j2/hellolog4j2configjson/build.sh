e730c6305fcdc266784b44cbee470812e21a1177
John Bannick <jbannick@7128.com>
2018-04-07 17:40:06 -0400

    Update build.sh

--- src/log4j2/hellolog4j2configjson/build.sh
+++ src/log4j2/hellolog4j2configjson/build.sh
@@ -2,7 +2,7 @@
 mkdir -p out
 
 javac -d out \
-    -cp ./src:$LOG4J2_HOME/log4j-core-2.10.0.jar:$LOG4J2_HOME/log4j-api-2.10.0.jar:$JSON_HOME/jackson-core-2.9.4.jar:$JSON_HOME/jackson-databind-2.9.4.jar:$JSON_HOME/jackson-annotations-2.9.4.jar \
+    -cp ./src:$LOG4J2_HOME/log4j-core-2.11.0.jar:$LOG4J2_HOME/log4j-api-2.11.0.jar:$JSON_HOME/jackson-core-2.9.0.jar:$JSON_HOME/jackson-databind-2.9.0.jar:$JSON_HOME/jackson-annotations-2.9.0.jar \
     src/HelloLog4J2ConfigJSON.java
 
 cp ./src/log4j2.json ./out

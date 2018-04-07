c918ae4e6009427a2def49d02915f593a328d39a
John Bannick <jbannick@7128.com>
2018-04-07 15:44:52 -0400

    Create FieldWithSubfields.java

---
+++ src/modules/hellomodulesusingnonmodlib2/src/kata/app/FieldWithSubfields.java
@@ -0,0 +1,17 @@
+package kata.app;
+
+import java.util.Arrays;
+
+public class FieldWithSubfields extends Field{
+
+    private Field[] subfields;
+
+    public FieldWithSubfields(final String name, final Field[] subfields) {
+        super(name);
+        this.subfields = Arrays.copyOf(subfields, subfields.length);
+    }
+
+    public Field[] getSubfields() {
+        return subfields;
+    }
+}

c1d6960ba97b3b8678a09e52986cef7e85cfdd2a
John Bannick <jbannick@7128.com>
2018-04-14 14:40:43 -0400

    Delete build.sh

--- src/csvparser/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p out
-mkdir -p lib
-
-cp $CSV_HOME/commons-csv-1.5.jar lib
-
-CP=lib/commons-csv-1.5.jar
-
-javac -cp $CP -d out -sourcepath src $(find src -name '*.java')

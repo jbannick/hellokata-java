0e934cc2be0993a7f6bf545c7ebfb8add5282f5a
John Bannick <jbannick@7128.com>
2018-04-14 17:28:24 -0400

    Create JSONWriter.java

---
+++ src/modules/hellomodulesmultilibs/src/appmod/kata/app/JSONWriter.java
@@ -0,0 +1,55 @@
+package kata.app;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bushe.swing.event.EventBus;
+import org.bushe.swing.event.EventTopicSubscriber;
+
+import static kata.app.CSVReader.MSG_STARTING;
+import static kata.app.CSVReader.MSG_STOPPING;
+import static kata.app.CSVReader.TOPIC_CSV2JSON;
+
+public class JSONWriter implements EventTopicSubscriber {
+
+    private static final Logger logger = LogManager.getLogger();
+    private ObjectMapper mapper = new ObjectMapper();
+    private Person person;
+
+    public JSONWriter() {
+        EventBus.subscribe(TOPIC_CSV2JSON, this);
+    }
+
+    public void onEvent(final String topic, final Object payload) {
+        if (TOPIC_CSV2JSON.equalsIgnoreCase(topic)) {
+            String message = payload.toString();
+            switch (message) {
+                case MSG_STARTING:
+                    logger.info(message);
+                    break;
+                case MSG_STOPPING:
+                    logger.info(message);
+                    break;
+                default:
+                    String[] msg = message.split(",");
+                    try {
+                        switch (msg[0]) {
+                            case "first_name":
+                                person = new Person(msg[1]);
+                                break;
+                            case "last_name":
+                                person.setLastName(msg[1]);
+                                logger.info(mapper.writeValueAsString(person));
+                                break;
+                            default:
+                                break;
+                        }
+                    } catch (JsonProcessingException jpe) {
+                        jpe.printStackTrace();
+                    }
+                    break;
+            }
+        }
+    }
+}

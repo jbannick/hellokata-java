b96874ba12fb0b9a218344ab548818a53e04eee3
John Bannick <jbannick@7128.com>
2018-04-14 11:31:23 -0400

    Update build.sh

--- src/log4j2/hellolog4j2mod/build.sh
+++ src/log4j2/hellolog4j2mod/build.sh
@@ -5,6 +5,6 @@ mkdir -p mods
 cp $LOG4J2_HOME/log4j-api-2.11.0.jar mods
 cp $LOG4J2_HOME/log4j-core-2.11.0.jar mods
 
-cp src/appmod/log4j2.xml out/appmod
+javac --module-path mods -d out --module-source-path src -m appmod
 
-javac -cp $CP --module-path mods -d out --module-source-path src -m appmod
+cp src/appmod/log4j2.xml out/appmod

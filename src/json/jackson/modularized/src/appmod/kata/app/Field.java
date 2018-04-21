67b154abdfc232393abe3c8fc94b783e1210d736
John Bannick <jbannick@7128.com>
2018-04-21 18:17:26 -0400

    Create Field.java

---
+++ src/json/jackson/modularized/src/appmod/kata/app/Field.java
@@ -0,0 +1,13 @@
+package kata.app;
+
+public class Field {
+    private String name;
+
+    public Field(final String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}

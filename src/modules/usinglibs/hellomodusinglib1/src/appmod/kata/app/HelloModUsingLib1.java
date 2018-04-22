1c2549154675b7d3451332d1309c2dcca84945b6
John Bannick <jbannick@7128.com>
2018-04-22 15:43:22 -0400

    Create HelloModUsingLib1.java

---
+++ src/modules/usinglibs/hellomodusinglib1/src/appmod/kata/app/HelloModUsingLib1.java
@@ -0,0 +1,14 @@
+package kata.app;
+
+public class HelloModUsingLib1 {
+   
+   public static void main(String... args) {
+      System.out.println("Hello Modularized Using Library 1!");
+
+      Publisher pub = new Publisher();
+      Subscriber sub = new Subscriber();
+
+      pub.publish("Pizza in the break room");
+   }
+   
+}

e3621d382b5e47807ce2cc18fd289a79627908af
John Bannick <jbannick@7128.com>
2018-04-22 16:48:08 -0400

    Create HelloModUsingLib2.java

---
+++ src/modules/usinglibs/hellomodusinglib2/src/appmod/kata/app/HelloModUsingLib2.java
@@ -0,0 +1,18 @@
+package kata.app;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class HelloModUsingLib2 {
+
+    private static final Logger logger = LogManager.getLogger();
+
+    public static void main(String... args){
+        System.out.println("Hello Modularized Using Library 2!");
+
+        logger.info("This is an information entry");
+        logger.debug("This is a debugging entry");
+        logger.warn("This is a warning entry");
+        logger.error("This is an error entry");
+    }
+}

3e4e922a3834119015cc25ce091481a97e1f5a03
John Bannick <jbannick@7128.com>
2018-04-25 04:57:27 -0400

    Update HelloModulesImage.java

--- src/modules/images/hellomodimages/src/appmod/kata/app/HelloModulesImage.java
+++ src/modules/images/hellomodimages/src/appmod/kata/app/HelloModulesImage.java
@@ -3,6 +3,6 @@ package kata.app;
 public class HelloModulesImage {
 
     public static void main(String... args) {
-        System.out.println("Hello Modules Image!");
+        System.out.println("Hello Modules Custom Image!");
     }
 }

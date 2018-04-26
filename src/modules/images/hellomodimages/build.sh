3953b41ae5938ef0fdea6395e8fb92b1bde4a9d1
John Bannick <jbannick@7128.com>
2018-04-26 04:52:03 -0400

    Update build.sh

--- src/modules/images/hellomodimages/build.sh
+++ src/modules/images/hellomodimages/build.sh
@@ -3,13 +3,25 @@ mkdir -p out
 mkdir -p mods
 
 javac -d out \
-    src/appmod/kata/app/HelloModulesImage.java \
+    src/appmod/kata/app/HelloModulesImages.java \
     src/appmod/module-info.java
 
-jar -cfe mods/appmod.jar kata.app.HelloModulesImage -C out .
+jar -cfe mods/appmod.jar kata.app.HelloModulesImages -C out .
 
 $JAVA_HOME/bin/jlink \
---module-path mods/:$JAVA_HOME/jmods \
+--module-path mods/:$JDK_MACOS_HOME/jmods \
 --add-modules appmod \
 --launcher appmod=appmod \
---output appmod-image
+--output appmod-image-macos
+
+$JAVA_HOME/bin/jlink \
+--module-path mods/:$JDK_WIN64_HOME/jmods \
+--add-modules appmod \
+--launcher appmod=appmod \
+--output appmod-image-win64
+
+$JAVA_HOME/bin/jlink \
+--module-path mods/:$JDK_LINUX_HOME/jmods \
+--add-modules appmod \
+--launcher appmod=appmod \
+--output appmod-image-linux

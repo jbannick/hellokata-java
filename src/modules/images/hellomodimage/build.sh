bad17d3976a8d8da4fb35cc083b7d1c1a3776241
John Bannick <jbannick@7128.com>
2018-04-26 04:32:32 -0400

    Create build.sh

---
+++ src/modules/images/hellomodimage/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+mkdir -p out
+mkdir -p mods
+
+javac -d out \
+    src/appmod/kata/app/HelloModulesImage.java \
+    src/appmod/module-info.java
+
+jar -cfe mods/appmod.jar kata.app.HelloModulesImage -C out .
+
+$JAVA_HOME/bin/jlink \
+--module-path mods/:$JAVA_HOME/jmods \
+--add-modules appmod \
+--launcher appmod=appmod \
+--output appmod-image

0f350e513deb9bcd273ad738c6ceef22d44c3af6
John Bannick <jbannick@7128.com>
2018-04-28 12:11:25 -0400

    Create HelloModImageUsingLib1.java

---
+++ src/modules/custom_images/hellomodimageusinglib1/src/appmod/kata/app/HelloModImageUsingLib1.java
@@ -0,0 +1,15 @@
+package kata.app;
+
+public class HelloModImageUsingLib1 {
+   
+   public static void main(String... args) {
+      System.out.println("Hello Modularized Image Using Library 1!");
+
+      Publisher  pub = new Publisher();
+
+      new Subscriber();
+
+      pub.publish("Pizza in the break room");
+   }
+   
+}

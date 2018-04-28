4694b787923c7edd411bc8299732d7eeb617afaf
John Bannick <jbannick@7128.com>
2018-04-28 13:47:54 -0400

    Delete build.sh

--- src/modules/hellomodules2/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p out
-
-javac -d out \
-    src/kata/modules/hmod/HelloModules.java \
-    src/kata/modules/hmod/foo/Foo.java \
-    src/org/fubar/Bar.java \
-    src/module-info.java

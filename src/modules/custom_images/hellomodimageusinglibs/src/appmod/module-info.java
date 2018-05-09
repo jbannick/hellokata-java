700d05c918f4b219e06f07bf5acb5322fec9d7e3
John Bannick <jbannick@7128.com>
2018-05-09 05:16:07 -0400

    Update module-info.java

--- src/modules/custom_images/hellomodimageusinglibs/src/appmod/module-info.java
+++ src/modules/custom_images/hellomodimageusinglibs/src/appmod/module-info.java
@@ -2,6 +2,7 @@ module appmod {
     requires eventbus;
     requires commons.csv;
     requires jackson.databind;
+    requires jackson.core;
     requires org.apache.logging.log4j;
 
     opens kata.app;

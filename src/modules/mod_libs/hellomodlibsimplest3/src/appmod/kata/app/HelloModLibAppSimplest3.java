4a725e7d3b0797162861b5a00b858a8cb9afe13e
John Bannick <jbannick@7128.com>
2018-05-20 15:36:35 -0400

    Create HelloModLibAppSimplest3.java

---
+++ src/modules/mod_libs/hellomodlibsimplest3/src/appmod/kata/app/HelloModLibAppSimplest3.java
@@ -0,0 +1,12 @@
+package kata.app;
+import kata.lib.HelloModLibSimplest;
+
+public class HelloModLibAppSimplest3 {
+
+    public static void main(String... args) {
+        System.out.println("Hello Custom Image Using a Library - Simplest!");
+
+        HelloModLibSimplest helloModLibSimplest = new HelloModLibSimplest();
+        helloModLibSimplest.doSomething();
+    }
+}

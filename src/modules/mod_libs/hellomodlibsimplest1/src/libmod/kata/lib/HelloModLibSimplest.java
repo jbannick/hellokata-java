9919f734c67e4c501b50a4dbabcb440191439968
John Bannick <jbannick@7128.com>
2018-05-20 10:22:42 -0400

    Create HelloModLibSimplest.java

---
+++ src/modules/mod_libs/hellomodlibsimplest1/src/libmod/kata/lib/HelloModLibSimplest.java
@@ -0,0 +1,12 @@
+package kata.lib;
+
+public class HelloModLibSimplest {
+    public HelloModLibSimplest() {
+
+    }
+
+    public void doSomething() {
+        System.out.println();
+        System.out.println("Hello from a Modularized Library!");
+    }
+}

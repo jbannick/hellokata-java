4019f086fadade90fc77bbfd37b02aedbf263492
John Bannick <jbannick@7128.com>
2018-05-20 10:23:09 -0400

    Create build.sh

---
+++ src/modules/mod_libs/hellomodlibsimplest1/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+mkdir -p out
+mkdir -p run
+
+# Build the modularized lib
+javac -d out/libmod \
+    src/libmod/kata/lib/HelloModLibSimplest.java \
+    src/libmod/module-info.java
+
+jar -cf libmod.jar -C out/libmod .
+
+# Build the non-modularized app
+javac -d out/app \
+    -cp libmod.jar \
+    src/app/kata/app/HelloModLibAppSimplest1.java
+
+jar -cfm app.jar src/app/META-INF/MANIFEST.MF -C out/app .

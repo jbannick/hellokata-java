a0347e9471da12669e580f125a70db5c4105c029
John Bannick <jbannick@7128.com>
2018-06-17 14:29:29 -0400

    Update build_app.sh

--- src/modules/custom_images/hellomodimageusinglibs/build_app.sh
+++ src/modules/custom_images/hellomodimageusinglibs/build_app.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env bash
-mkdir -p out
-mkdir -p mods
-mkdir -p libs
-mkdir -p work
-
-rm -rf appmod-image
-
-ROOT_DIR=$PWD
-
-. build_lib1.sh
-. build_lib2.sh
-. build_lib3.sh
-. build_lib4.sh
 
 javac --module-path mods -d out \
     src/appmod/kata/app/HelloModImageUsingLibs.java \

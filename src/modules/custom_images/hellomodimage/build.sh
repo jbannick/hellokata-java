a5bd3d23aa8d29e791051b17899ce9596983461b
John Bannick <jbannick@7128.com>
2018-06-17 14:15:58 -0400

    Update build.sh

--- src/modules/custom_images/hellomodimage/build.sh
+++ src/modules/custom_images/hellomodimage/build.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
+rm -rf out
+rm -rf mods
+
 mkdir -p out
 mkdir -p mods
 
 rm -rf appmod-image
 
 javac -d out \
-    src/appmod/kata/app/HelloModulesImage.java \
-    src/appmod/module-info.java
+src/appmod/kata/app/HelloModulesImage.java \
+src/appmod/module-info.java
 
 jar -cfe mods/appmod.jar kata.app.HelloModulesImage -C out .
 

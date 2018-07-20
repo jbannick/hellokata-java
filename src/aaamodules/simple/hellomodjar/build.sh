829ef697efc6cfd34762b1c67b6c3cd1657e2872
John Bannick <jbannick@7128.com>
2018-07-20 05:08:51 -0400

    Update build.sh

--- src/aaamodules/simple/hellomodjar/build.sh
+++ src/aaamodules/simple/hellomodjar/build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+rm -f *.jar
+rm -rf out
+
 mkdir -p out
 
-javac -d out/appmod \
+javac --module-path . -d out --module-source-path src -m appmod\
     src/appmod/kata/app/HelloModJAR.java \
     src/appmod/module-info.java
 

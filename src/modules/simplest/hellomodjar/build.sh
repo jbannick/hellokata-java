a2f17ea6d603db1832b747d8e1a9883778126e99
John Bannick <jbannick@7128.com>
2018-07-23 04:48:10 -0400

    Delete build.sh

--- src/modules/simplest/hellomodjar/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p out
-mkdir -p mods
-
-javac -d out/appmod \
-    src/appmod/kata/app/HelloModJAR.java \
-    src/appmod/module-info.java
-
-jar -cfe mods/appmod.jar kata.app.HelloModJAR -C out/appmod .

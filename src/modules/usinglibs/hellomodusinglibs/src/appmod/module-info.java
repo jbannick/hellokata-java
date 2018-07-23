db258f60977801e3ead8c385d96d4ad72458f0d8
John Bannick <jbannick@7128.com>
2018-07-23 04:44:48 -0400

    Delete module-info.java

--- src/modules/usinglibs/hellomodusinglibs/src/appmod/module-info.java
+++
@@ -1,9 +0,0 @@
-module appmod {
-    requires org.apache.logging.log4j;
-    requires eventbus;
-    requires commons.csv;
-    requires jackson.databind;
-    requires jackson.core;
-
-    opens kata.app;
-}
